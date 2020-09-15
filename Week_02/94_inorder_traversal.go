cacbd0cb4ee5cb9b422a019e8bb2ee9fabfc99fa
alex <304704787@qq.com>
2020-09-15 17:05:17 +0800

    中序遍历

---
+++ Week_02/94_inorder_traversal.go
@@ -0,0 +1,20 @@
+package Week_02
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	result := make([]int, 0, 3)
+	result = append(result, inorderTraversal(root.Left)...)
+	result = append(result, root.Val)
+	result = append(result, inorderTraversal(root.Right)...)
+
+	return result
+}
